5389e970683a1cc6631e9b6ce904c5804c6dcbc7
Your Name <khtran68@gmail.com>
2022-05-26 20:28:29 -0500

    Mobile Responsive

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const nav = document.getElementById('nav');
-const LIGHT_THEME = 'light'
-const DARK_THEME = 'dark'
+const LIGHT_THEME = 'light';
+const DARK_THEME = 'dark';
+var width,height;
+
+// Menu Elements
+const switchWrapper = document.getElementById('theme-switch-wrapper');
+const menuBars = document.getElementById('menu-bars');
+const overlay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
 
 
 // Dark or light images
@@ -55,8 +65,48 @@ function loadTheme(currentTheme){
     }
 }
 
+function toggleNav(){
+    //toggle: Menu Bars Open/Closed
+    menuBars.classList.toggle('change');
+    // toggle: Menu Active
+    overlay.classList.toggle('overlay-active');
+    if(overlay.classList.contains('overlay-active')){
+        // Animate In - Overlay
+        overlay.classList.remove('overlay-slide-left');
+        overlay.classList.add('overlay-slide-right');
+        switchWrapper.classList.remove('overlay-slide-left');
+        switchWrapper.classList.add('overlay-slide-right');
+    }else{
+        // Animate Out - Overlay
+        overlay.classList.remove('overlay-slide-right');
+        overlay.classList.add('overlay-slide-left');    
+        switchWrapper.classList.remove('overlay-slide-right');
+        switchWrapper.classList.add('overlay-slide-left');
+    }
+}
+
 // Event Listener
 toggleSwitch.addEventListener('change', swtichTheme);
+menuBars.addEventListener('click', toggleNav);
+nav1.addEventListener('click', toggleNav);
+nav2.addEventListener('click', toggleNav);
+nav3.addEventListener('click', toggleNav);
+nav4.addEventListener('click', toggleNav);
+
+window.onresize = window.onload = function() {
+    width = this.innerWidth;
+    height = this.innerHeight;
+    if(width >= 1025){
+        console.log("change");
+        overlay.classList.remove('overlay-slide-left'); 
+        overlay.classList.remove('overlay-slide-right');
+        switchWrapper.classList.remove('overlay-slide-left');
+        switchWrapper.classList.remove('overlay-slide-right');
+        menuBars.classList.remove('change');
+    }
+    //document.body.innerHTML = width + 'x' + height; // For demo purposes
+}
+
 
 // Get Local Storage Theme
 const currentTheme = localStorage.getItem('theme');
